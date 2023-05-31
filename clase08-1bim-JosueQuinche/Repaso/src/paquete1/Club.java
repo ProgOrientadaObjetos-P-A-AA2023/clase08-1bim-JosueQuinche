5bd1543d6457586c4aecbef20ddf6dc5809bb735
JosueQuinche <116389973+JosueQuinche@users.noreply.github.com>
2023-05-31 13:02:07 -0500

    Add files via upload

---
+++ clase08-1bim-JosueQuinche/Repaso/src/paquete1/Club.java
@@ -0,0 +1,39 @@
+package paquete1;
+import java.io.Serializable;
+public class Club implements Serializable{
+
+    private int fundacion;
+    private String nombre;
+    private String siglas;
+    
+    public Club(String n, String si, int f){
+        fundacion = f;
+        nombre = n;
+        siglas = si;
+    }
+    
+    public void establecerFundacion(int n){
+        fundacion = n;
+    }
+    
+    public void establecerNombre(String n){
+        nombre = n;
+    }
+    
+    public void establecerSiglas(String n){
+        siglas = n;
+    }
+    
+    public int obtenerFundacion(){
+        return fundacion;
+    }
+    
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public String obtenerSiglas(){
+        return siglas;
+    }
+    
+}
---
+++ clase08-1bim-JosueQuinche/Repaso/src/paquete1/Jugador.java
@@ -0,0 +1,38 @@
+package paquete1;
+import java.io.Serializable;
+public class Jugador implements Serializable{
+    private String nombre;
+    private int dorsal;
+    private Club club;
+    
+    public Jugador(String nom, int d, Club c){
+        nombre = nom;
+        dorsal = d;
+        club = c;
+    }
+    
+    public void establecerNombre(String n){
+        nombre = n;
+    }
+    
+    public void establecerDorsal(int n){
+        dorsal = n;
+    }
+    
+    public void establecerClub(Club n){
+        club = n;
+    }
+    
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public int obtenerDorsal(){
+        return dorsal;
+    }
+    
+    public Club obtenerClub(){
+        return club;
+    }
+    
+}
